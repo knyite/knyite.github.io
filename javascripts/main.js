0425816e402f02cd44e0a9f56b0cd12d08423cfc
daclouds <daclouds@gmail.com>
2015-02-07 22:03:30 +0900

    slide menu (javascript)

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+$(document).ready(function() {
+	
+	$('.menu').hover(function() {
+		$('.menu').animate({ top: -100 }, 1000);
+		$('.menudown').animate({ top: 22 }, 1000);
+	});
+
+	$('.menudown').hover(function() {
+		$('.menu').animate({ top: -100 }, 1000);
+		$('.menudown').animate({ top: 22 }, 1000);
+	}
+	, function() {
+		$('.menu').animate({ top: 22 }, 1000);
+		$('.menudown').animate({ top: -100 }, 1000);
+	});	
+});
