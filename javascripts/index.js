9b376965d27e5d1be3220814816a6a33cf65f713
knyite <knyite@gmail.com>
2015-03-04 22:50:57 +0900

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	stylesheets/stylesheet.css

---
+++ javascripts/index.js
@@ -0,0 +1,30 @@
+jQuery(function($) {
+    $('#btnResume').click(function() {
+        $('#resumeBlock').css('visibility', 'visible');
+        $('#resume').css('visibility', 'visible');
+    });
+
+    $('#exit').click(function() {
+        $('#resumeBlock').css('visibility', 'hidden');
+        $('#resume').css('visibility', 'hidden');
+    });
+
+    $(document).keyup(function(e) {
+        if (e.keyCode == 27) $('#exit').click();   // esc
+    });
+
+    document.getElementById("images").style.overflowX = "scroll";
+
+    $("#imageButtons a").click(function (ev) {
+        ev.preventDefault();
+        $('#work ul').scrollTo(this.hash, {duration:'slow'});
+    });
+
+    $("#menu").click(function() {
+        $("#fullMenu").css('display', 'block');
+    });
+    
+    $("#fullMenuExit").click(function() {
+        $("#fullMenu").css('display', 'none');
+    });
+});
